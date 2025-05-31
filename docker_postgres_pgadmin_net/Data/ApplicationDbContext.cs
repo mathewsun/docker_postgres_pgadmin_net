27e5bd2bf5fe0e353e1ba55817894a5649a9ebe0
Matvey Dubrovin <mathewsun@rambler.ru>
2025-06-01 01:13:08 +0300

    add dbcontext

---
+++ docker_postgres_pgadmin_net/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using docker_postgres_pgadmin_net.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace docker_postgres_pgadmin_net.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities => Set<City>();
+    }
+}
